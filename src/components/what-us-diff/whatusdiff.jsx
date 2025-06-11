40a8dd4d3b306d74458bf77c05458f4bf05d7960
fullstackdeveloper918 <fullstackdeveloper918@gmail.com>
2025-06-11 19:38:02 +0530

    add video on pain section

--- src/components/what-us-diff/whatusdiff.jsx
+++ src/components/what-us-diff/whatusdiff.jsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import whatusImage3 from "../../../public/images/1.svg";
 import whatusImage4 from "../../../public/images/4.svg";
 import whatusImage5 from "../../../public/images/5.svg";
-import  Link  from "next/link";
+import Link from "next/link";
 import { Rocket } from "lucide-react";
 gsap.registerPlugin(ScrollTrigger);
 function PainSection() {
@@ -41,28 +41,29 @@ function PainSection() {
     },
   ];
   const duplicatedCards = [...cards, ...cards];
-//   useEffect(() => {
-//   if (showSecondSection) {
-//     const ctx = gsap.context(() => {
-//       const tl = gsap.timeline();
-//       tl.from(".title-heading", {
-//         y: 90,
-//         opacity: 0,
-//         duration: 1.8,
-//         ease: "power3.out",
-//       });
-//       tl.from(".card-item", {
-//         y: 70,
-//         opacity: 0,
-//         duration: 0.5,
-//         ease: "power3.out",
-//         stagger: 0.1,
-//         pin: true,
-//       }, "-=0.4"); // overlaps slightly with previous
-//     }, cardsSectionRef);
-//     return () => ctx.revert();
-//   }
-// }, [showSecondSection]);
+  //   useEffect(() => {
+  //   if (showSecondSection) {
+  //     const ctx = gsap.context(() => {
+  //       const tl = gsap.timeline();
+  //       tl.from(".title-heading", {
+  //         y: 90,
+  //         opacity: 0,
+  //         duration: 1.8,
+  //         ease: "power3.out",
+  //       });
+  //       tl.from(".card-item", {
+  //         y: 70,
+  //         opacity: 0,
+  //         duration: 0.5,
+  //         ease: "power3.out",
+  //         stagger: 0.1,
+  //         pin: true,
+  //       }, "-=0.4"); // overlaps slightly with previous
+  //     }, cardsSectionRef);
+  //     return () => ctx.revert();
+  //   }
+  // }, [showSecondSection]);
+
   useEffect(() => {
     const ctx = gsap.context(() => {
       const tl = gsap.timeline({
@@ -79,8 +80,8 @@ function PainSection() {
       });
       tl.to(titleRef.current, {
         scale: 1.8,
-        duration:1.5,
-        fontSize: "1000vw",
+        duration: 1.5,
+        fontSize: "500vw",
         color: "#fff",
         transformOrigin: "51% center",
         ease: "power3.out",
@@ -101,68 +102,76 @@ function PainSection() {
         </section>
       </div>
       {showSecondSection && (
-  <section
-    ref={cardsSectionRef}
-    data-aos="zoom-out-up"
-    className="absolute top-[576px] inset-0 flex flex-col items-center justify-center bg-black text-white md:p-10 p-2 overflow-hidden"
-
-
-  >
-    <h2 className="title-heading text-white font-bold text-center z-10 xl:mb-20 md:mb-12 mb-10 xxl:text-7xl xl:text-5xl text-3xl">
-      What Makes Us Different?
-    </h2>
-    <div className="w-full sliderSec">
-      <div
-        className="flex space-x-8 w-max animate-scroll-x"
-        style={{ animation: "scrollLeft 50s linear infinite" }}
-      >
-        {duplicatedCards?.map((card, index) => (
-          <div
-            key={index}
-            className="card-item flex-shrink-0 w-[400px] h-96 bg-black border border-white/20 backdrop-blur-sm rounded-2xl p-8 transition-colors duration-300 slidecol"
-          >
-            <div className="flex items-center mb-6">
-              <Image src={card.icon} width={60} height={60} />
+        <section
+          ref={cardsSectionRef}
+          data-aos="zoom-out-up"
+          className="absolute top-[576px] inset-0 flex flex-col items-center justify-center bg-black text-white md:p-10 p-2 overflow-hidden"
+        >
+          <video
+            className="absolute top-0 left-0 w-full h-full object-cover -z-10"
+            autoPlay
+            loop
+            muted
+            playsInline
+            src="/pain.mp4"
+            // Or use source elements if needed
+            // preload="auto"
+          />
+          <div className="absolute top-0 left-0 w-full h-full bg-black/70  -z-10"></div>
+          <h2 className="title-heading text-white font-bold text-center z-10 xl:mb-20 md:mb-12 mb-10 xxl:text-7xl xl:text-5xl text-3xl">
+            What Makes Us Different?
+          </h2>
+          <div className="w-full sliderSec">
+            <div
+              className="flex space-x-8 w-max animate-scroll-x"
+              style={{ animation: "scrollLeft 50s linear infinite" }}
+            >
+              {duplicatedCards?.map((card, index) => (
+                <div
+                  key={index}
+                  className="card-item flex-shrink-0 w-[400px] h-96 bg-black/10 backdrop-blur-md rounded-2xl p-8 transition-colors duration-300 slidecol"
+                  style={{ border: "1px solid #dddddd38" }}
+                >
+                  <div className="flex items-center mb-6">
+                    <Image src={card.icon} width={60} height={60} />
+                  </div>
+                  <h3 className="text-white md:mb-4 mb-2 cardtitile">
+                    {card.title}
+                  </h3>
+                  <p className="text-gray-300 cardDecription">
+                    {card.description}
+                  </p>
+                </div>
+              ))}
             </div>
-            <h3 className="text-white md:mb-4 mb-2 cardtitile">
-              {card.title}
-            </h3>
-            <p className="text-gray-400 cardDecription">
-              {card.description}
-            </p>
-          </div>
-        ))}
-      </div>
-       <button className="relative z-[9999] bannerbtn mt-14 mx-auto flex items-center justify-center">
-                    <Link className="header-button ajax-link" href="/contact-us">
-                      <div className="button-icon-link right">
-                        <div className="icon-wrap-scale">
-                          <div className="icon-wrap parallax-wrap">
-                            <div className="button-icon parallax-element">
-                              {/* <i className="fa-solid fa-arrow-right"></i> */}
-                              <Rocket className="ml-2 h-5 w-5" />
-                            </div>
-                          </div>
-                        </div>
-                        <div className="button-text sticky right">
-                          {/* <span data-hover="Let's Talk">Start Your Mission <Rocket className="ml-2 h-5 w-5" /></span> */}
-                          <span data-hover="Let's Talk">
-                            Start Your Mission
-                          </span>
-                        </div>
+            <button className="relative z-[9999] bannerbtn mt-14 mx-auto flex items-center justify-center">
+              <Link className="header-button ajax-link" href="/contact-us">
+                <div className="button-icon-link right">
+                  <div className="icon-wrap-scale">
+                    <div className="icon-wrap parallax-wrap">
+                      <div className="button-icon parallax-element">
+                        {/* <i className="fa-solid fa-arrow-right"></i> */}
+                        <Rocket className="ml-2 h-5 w-5" />
                       </div>
-                    </Link>
-                    {/* <Link
+                    </div>
+                  </div>
+                  <div className="button-text sticky right">
+                    {/* <span data-hover="Let's Talk">Start Your Mission <Rocket className="ml-2 h-5 w-5" /></span> */}
+                    <span data-hover="Let's Talk">Start Your Mission</span>
+                  </div>
+                </div>
+              </Link>
+              {/* <Link
                       href="/contact-us"
                       id="btnTwo"
                       className="BtnTwo btnWrapper rounded-[50px] text-white px-6 py-3 flex items-center justify-center"
                     >
                       Start Your Mission <Rocket className="ml-2 h-5 w-5" />
                     </Link> */}
-                  </button>
-    </div>
-  </section>
-)}
+            </button>
+          </div>
+        </section>
+      )}
     </div>
   );
 }
